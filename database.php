add6597ec17ed69ef8b47b478bdbeca6ec7633fd
Faan4T <Faan4t@outlook.com>
2023-04-17 20:55:50 +0500

    update_from_staging_local

--- database.php
+++ database.php
@@ -1,4 +1,11 @@
-<?php
+<!--
+                                $hostname = "localhost";
+                                $username = "local_user";
+                                $password = "C5ou01{c8H_]";
+                                $database = "koltext_db";
+                                $link = mysqli_connect($hostname, $username, $password, $database);
+                             -->
+                            <?php
                                 $hostname = "localhost";
                                 $username = "local_user";
                                 $password = "C5ou01{c8H_]";
